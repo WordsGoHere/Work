5b4c9712fa74f213d7aee67fef25944753f87006
CG <good2flowz@gmail.com>
2016-05-25 13:10:58 -0700

    Pushing to branch'

--- BlackJack.rb
+++ BlackJack.rb
@@ -20,7 +20,6 @@ def deal(player)
   delete_from_deck(card, suit)
   card = ace_check(card, player)
   player << [suit, card]
-<<<<<<< HEAD
   Deck[suit].delete_if { | _ , value| value == card }
 end
 
@@ -28,16 +27,6 @@ def player_ace(card,player)
   prompt"You have an Ace"
   prompt"Is that an 11 or a 1?"
   answer = gets.chomp
-=======
-  Deck[suit].delete_if{|key, value| value == card}  #This was not working correctly on my system
-end
-
-def ace_check(card, player) 
-  if card == '1' && player == play_h  # I was having trouble establishing a variable to call here
-    prompt"You have an Ace"           # it does work if you remove the  player == play_h component
-    prompt"Is that an 11 or a 1?"     
-    answer= gets.chomp
->>>>>>> 31fcc07885aaafeb519d07046c1d2c5120a0c9e2
     if answer == '1'
       card = 1
     else
@@ -59,26 +48,24 @@ def ace_check(card, player)
   end
 end
 
-<<<<<<< HEAD
 def delete(arg)
   arg == nil?
 end
-=======
-def delete(arg)  #Had to make this for my argument to work on my local
-  arg == nil     #system it was returing the numbers fine but when I moved this
-end              #file to cloud 9 it was returning a nil value for the card.
->>>>>>> 31fcc07885aaafeb519d07046c1d2c5120a0c9e2
 
 def delete_from_deck(suit, card)
   Deck[suit].delete(card)
 end
+
  # rubocop disable: AllCops
+ 
 def add_hand(array) 
   array.flatten.delete_if { |word| word == "Hearts" || word == "Spades" ||
   word == "Clubs" || word == "Diamond" }.map { |num| num.to_i }.inject 
   { |num| num += }
 end
+
  # rubocop enable: AllCops
+ 
 def winner_check(human, computer)
 x = 0
 y = 0
@@ -92,17 +79,10 @@ case
     (Busted?(human) && y == 4)
     prompt "The Computer won that time."
     return 7
-<<<<<<< HEAD
   when (bigger(human, computer) && !Busted?(human)) || Busted?(computer)
     prompt "Player Won"
     x += 1
   when (bigger(computer,human) && !Busted?(computer)) || Busted?(human)
-=======
-  elsif (bigger(human, computer) && !Busted?(human)) || Busted?(computer)
-    prompt "Player Won"
-    x += 1
-  elsif (bigger(computer,human) && !Busted?(computer)) || Busted?(human)
->>>>>>> 31fcc07885aaafeb519d07046c1d2c5120a0c9e2
     prompt "Computer Won"
     y += 1
   else
@@ -126,9 +106,9 @@ loop do
 
   prompt"Welcome to Black Jack!"
 
-play_h = []       #To get my ace_check method to work I pulled these variables out of the loop 
-comp_h = []       #it still allowed the program to run but I still couldn't call play_h from
-                  # ace_check.
+play_h = []
+comp_h = []
+
 deal(play_h)
 deal(play_h)
 

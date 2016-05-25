bb5a5364eab4531e9c8d7d79bfdf9723a67ccec4
CG <good2flowz@gmail.com>
2016-05-25 13:38:24 -0700

    21 game testing

--- BlackJack.rb
+++ BlackJack.rb
@@ -1,12 +1,14 @@
-Deck = {'Hearts' => ['K10', 'Q10', 'J10', '10', '9', '8', '7', '6', '5', '3', '2', '1'],
-        'Spades' => ['K10', 'Q10', 'J10', '10', '9', '8', '7', '6', '5', '3', '2', '1'],
-        'Clubs' => ['K10', 'Q10', 'J10', '10', '9', '8', '7', '6', '5', '3', '2', '1'],
-        'Diamond' => ['K10', 'Q10', 'J10', '10', '9', '8', '7', '6', '5', '3', '2', '1']}
+Deck = { 'Hearts' => ['K10', 'Q10', 'J10', '10', '9', '8', '7',
+        '6', '5', '3', '2', '1'], 'Spades' => ['K10', 'Q10', 'J10',
+        '10', '9', '8', '7', '6', '5', '3', '2', '1'], 'Clubs' => 
+        ['K10', 'Q10', 'J10', '10', '9', '8', '7', '6', '5', '3',
+        '2', '1'], 'Diamond' => ['K10', 'Q10', 'J10', '10', '9', '8',
+        '7', '6', '5', '3', '2', '1'] }.Freeze
 
-BLACKJACK = 21
+BLACKJACK = 21.Freeze
 
 public
-      
+
 def prompt(message)
   puts " => #{message}"
 end
@@ -18,19 +20,23 @@ def deal(player)
   delete_from_deck(card, suit)
   card = ace_check(card, player)
   player << [suit, card]
-  Deck[suit].delete_if{|key, value| value == card}  #This was not working correctly on my system
+  Deck[suit].delete_if { | _ , value| value == card }
 end
 
-def ace_check(card, player) 
-  if card == '1' && player == play_h  # I was having trouble establishing a variable to call here
-    prompt"You have an Ace"           # it does work if you remove the  player == play_h component
-    prompt"Is that an 11 or a 1?"     
-    answer= gets.chomp
+def player_ace(card,player)
+  prompt"You have an Ace"
+  prompt"Is that an 11 or a 1?"
+  answer = gets.chomp
     if answer == '1'
       card = 1
     else
       card = 11
     end
+end
+
+def ace_check(card, player)
+  if card == '1' && player == play_h
+    player_ace(card, player)
   elsif card == '1'
     if greater_than(11, add_hand(player))
       card = 11
@@ -42,32 +48,41 @@ def ace_check(card, player)
   end
 end
 
-def delete(arg)  #Had to make this for my argument to work on my local
-  arg == nil     #system it was returing the numbers fine but when I moved this
-end              #file to cloud 9 it was returning a nil value for the card.
+def delete(arg)
+  arg == nil?
+end
 
-def delete_from_deck(suit,card)
+def delete_from_deck(suit, card)
   Deck[suit].delete(card)
 end
-  
+
+ # rubocop disable: AllCops
+ 
 def add_hand(array) 
-  array.flatten.delete_if{|word| word == "Hearts" || word == "Spades" ||
-  word == "Clubs" || word == "Diamond"}.map{|num| num.to_i}.inject{|num, result| result = result + num}
+  array.flatten.delete_if { |word| word == "Hearts" || word == "Spades" ||
+  word == "Clubs" || word == "Diamond" }.map { |num| num.to_i }.inject 
+  { |num| num += }
 end
 
+ # rubocop enable: AllCops
+ 
 def winner_check(human, computer)
 x = 0
 y = 0
-  if (x == 4 && bigger(human, computer) && !Busted?(human)) || (Busted?(computer) && x == 4)
+case 
+  when (x == 4 && bigger(human, computer) && !Busted?(human)) ||
+    (Busted?(computer) && x == 4)
     prompt "You are the Winner!"
     return 8
-  elsif (y == 4 && bigger(computer, human) && !Busted?(computer)) || (Busted?(human) && y == 4)
+  when
+    (y == 4 && bigger(computer, human) && !Busted?(computer)) ||
+    (Busted?(human) && y == 4)
     prompt "The Computer won that time."
     return 7
-  elsif (bigger(human, computer) && !Busted?(human)) || Busted?(computer)
+  when (bigger(human, computer) && !Busted?(human)) || Busted?(computer)
     prompt "Player Won"
     x += 1
-  elsif (bigger(computer,human) && !Busted?(computer)) || Busted?(human)
+  when (bigger(computer,human) && !Busted?(computer)) || Busted?(human)
     prompt "Computer Won"
     y += 1
   else
@@ -89,11 +104,11 @@ end
 
 loop do
 
-prompt"Welcome to Black Jack!"
+  prompt"Welcome to Black Jack!"
 
-play_h = []       #To get my ace_check method to work I pulled these variables out of the loop 
-comp_h = []       #it still allowed the program to run but I still couldn't call play_h from
-                  # ace_check.
+play_h = []
+comp_h = []
+
 deal(play_h)
 deal(play_h)
 
@@ -106,9 +121,9 @@ answer = gets.chomp
   case answer.downcase
     when 'hit'
       deal(play_h)
-    when 'stay' 
+    when 'stay'
       break
-    else 
+    else
       prompt"Pick either Stay or Hit"
     end
 prompt"You have #{play_h}"
@@ -134,7 +149,7 @@ end
 prompt"You had #{play_h}, the Computer had #{comp_h}"
 
 break if winner_check(play_h, comp_h) == (7..8)
-
+ # rubocop disable: AllCops
 prompt"Do you want to Play again? Yes or No"
 answer = gets.chomp
 break if answer.downcase.split.first('n')
